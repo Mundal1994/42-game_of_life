da6cebc9fa76a4e589101c03cd741b71c2b402a2
molesen <molesen@student.hive.fi>
2022-06-22 17:59:39 +0300

    made more uptimiztions to the speed by cleaning out the code

--- srcs/algorithm.c
+++ srcs/algorithm.c
@@ -1,13 +1,12 @@
 #include "../includes/life.h"
 
-static int	count_live_neighbors(t_data *data, int i, int j)
+static void	count_live_neighbors(t_data *data, int i, int j, int *count)
 {
 	int	end_x;
 	int	end_y;
 	int	x;
 	int	y;
 	int	start_x;
-	int	count;
 
 	x = j - 1;
 	y = i - 1;
@@ -18,43 +17,41 @@ static int	count_live_neighbors(t_data *data, int i, int j)
 	if (y < 0)
 		y = 0;
 	start_x = x;
-	count = 0;
-	while (y < data->total_y && y < end_y)
+	if (end_y > data->total_y)
+		end_y = data->total_y;
+	if (end_x > data->total_x)
+		end_x = data->total_x;
+	while (y < end_y)
 	{
 		x = start_x;
-		while (x < data->total_x && x < end_x)
+		while (x < end_x)
 		{
 			if (x == j && y == i)
 				++x;
 			if (data->map[y][x] == 'X')
-				++count;
+				(*count)++;
 			++x;
 		}
+		if (*count > 3)
+			break ;
 		++y;
 	}
-	return (count);
 }
 
-static int	check_live_cell(t_data *data, int y, int x)
+static int	check_live_cell(t_data *data, int y, int x, int count)
 {
-	int count;
-
-	count = count_live_neighbors(data, y, x);
+	count_live_neighbors(data, y, x, &count);
 	if (count == 2 || count == 3)
 		return (TRUE);
-	else
-		return (FALSE);
+	return (FALSE);
 }
 
-static int	check_dead_cell(t_data *data, int y, int x)
+static int	check_dead_cell(t_data *data, int y, int x, int count)
 {
-	int count;
-
-	count = count_live_neighbors(data, y, x);
+	count_live_neighbors(data, y, x, &count);
 	if (count == 3)
 		return (TRUE);
-	else
-		return (FALSE);
+	return (FALSE);
 }
 
 void	algorithm(t_data *data)
@@ -70,17 +67,13 @@ void	algorithm(t_data *data)
 		{
 			if (data->map[i][j] == '.')
 			{
-				if (check_dead_cell(data, i, j) == TRUE)
-				{
+				if (check_dead_cell(data, i, j, 0) == TRUE)
 					data->temp[i][j] = 'X';
-				}
 			}
 			else
 			{
-				if (check_live_cell(data, i, j) == FALSE)
-				{
+				if (check_live_cell(data, i, j, 0) == FALSE)
 					data->temp[i][j] = '.';
-				}
 			}
 			++j;
 		}
